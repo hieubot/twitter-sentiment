12f278693fc591bf26773801a58bea92c8fcc451
hieubot <52224285+hieubot@users.noreply.github.com>
2020-10-26 22:26:23 +0900

    started train/ eval files

---
+++ sentiment/evaluate_model.py
@@ -0,0 +1,26 @@
+import torch
+import numpy as np
+
+
+def evaluate_model(model, data_loader, loss_fn, device, n_examples):
+
+    model = model.eval()
+    losses = []
+    correct_predictions = 0
+
+    with torch.no_grad():
+        for d in data_loader:
+
+            input_ids = d["input_ids"].to(device)
+            attention_mask = d["attention_mask"].to(device)
+            targets = d["targets"].to(device)
+
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+
+            _, predictions = torch.max(outputs, dim=1)
+            loss = loss_fn(outputs, targets)
+
+            correct_predictions += torch.sum(predictions == targets)
+            losses.append(loss.item())
+
+    return correct_predictions.double() / n_examples, np.mean(losses)
